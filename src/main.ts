00deada098d1ff8b3ab02c09d0d2cd30ede984a5
Suthep <suthep@iapp.co.th>
2025-09-05 16:22:17 +0700

    เพิ่ม app.enableCors และ api document

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
+    maxAge: 86400,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
